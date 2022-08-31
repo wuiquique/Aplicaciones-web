5a6b591b26750e43ce8caf24e80ee4ca616d6422
wuiquique <luisenriquem15@gmail.com>
2022-08-31 16:23:26 -0600

    a

--- Lab4_Luis_Menendez/scripts/script.js
+++ Lab4_Luis_Menendez/scripts/script.js
@@ -1,12 +1,71 @@
 new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data () {
-      return {
-        min: 10,
-        max: 80,
-        slider: 18,
-        dialog: false,
+  el: "#app",
+  vuetify: new Vuetify(),
+  data() {
+    return {
+      min: 10,
+      max: 99,
+      dialog: false,
+      items: ["Male", "Female", "Other"],
+      firstname: "",
+      lastname: "",
+      gender: "",
+      age: 18,
+      phone: "",
+      email: "",
+      message: "",
+      titulod: "",
+      textod: "",
+      lista: [],
+      headers: [
+        {
+          text: "First Name",
+          align: "start",
+          sortable: false,
+          value: "firstname",
+        },
+        { text: "Last Name", value: "lastname" },
+        { text: "Gender", value: "gender" },
+        { text: "Age", value: "age" },
+        { text: "Phone Number", value: "phone" },
+        { text: "E-mail", value: "email" },
+        { text: "Message", value: "message" },
+      ],
+    };
+  },
+  methods: {
+    edad() {
+      if (this.age < 18) {
+        this.dialog = true;
+        this.titulod = "Oops...";
+        this.textod = "You can't use this functionality if you are underaged.";
+      } else {
+        this.dialog = true;
+        this.titulod = "Nice!";
+        this.textod =
+          "Your information has been sent. Espect a reply as soon as possible.";
+        this.lista.push({
+          firstname: this.firstname,
+          lastname: this.lastname,
+          gender: this.gender,
+          age: this.age,
+          phone: '+502' + this.phone,
+          email: this.email,
+          message: this.message,
+        });
+        this.firstname = "";
+        this.lastname = "";
+        this.gender = "";
+        this.age = 18;
+        this.phone = "";
+        this.email = "";
+        this.message = "";
+        this.$refs.form.resetValidation()
       }
     },
-  })
+    getColor(edad) {
+      if (edad < 35) return "light-blue";
+      else return "grey";
+    },
+  },
+});
